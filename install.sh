e7dfab986faf14c1d4f2ea27c93add7c1a44003a
Davor <dserfez@gmail.com>
2015-12-23 22:02:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O cloud-config.yml https://raw.githubusercontent.com/dserfez/tox-lab-net/master/cc.yml
-sudo coreos-install -d /dev/sda -c cloud-config.yml
+wget -O /home/core/cloud-config.yml https://raw.githubusercontent.com/dserfez/tox-lab-net/master/cc.yml
+sudo coreos-install -d /dev/sda -c /home/core/cloud-config.yml
 
